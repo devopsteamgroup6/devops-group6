02e931cc1d8e3761aec8499d73816876bbaf4878
Maaz Mukhtar <maazmukhtar95@gmail.com>
2024-10-16 15:41:53 +0500

    adding comments to App

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -2,9 +2,10 @@ package com.napier.devops;
 
 public class App
 {
+    //print out our test program
     public static void main(String[] args)
     {
         System.out.println("Boo yah!");
     }
-    //test
+
 }
