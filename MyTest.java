657e6c2758d04fcb42c197b7f9366c2c39a14ab7
devopstreamgroup6 <devopsteamgroup6@gmail.com>
2024-12-04 16:51:59 +0500

    Add files via upload

---
+++ MyTest.java
@@ -0,0 +1,10 @@
+mysql</groupId>
+            <artifactId>mysql-connector-java</artifactId>
+            <version>8.0.32</version>class MyTest
+{
+    @Test
+    void unitTest()
+    {
+        assertEquals(5, 5);
+    }
+}
