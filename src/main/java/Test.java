1ee495e041fcfda76f6329dcba2099874a4b5aa1
Maaz Mukhtar <maazmukhtar95@gmail.com>
2024-12-04 18:29:26 +0500

    pom file update + testing fixes

---
+++ src/main/java/Test.java
@@ -0,0 +1,16 @@
+
+//import org.junit.jupiter.api.*;
+import org.testng.annotations.Test;
+
+//import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.AssertJUnit.assertEquals;
+
+
+class MyTest
+{
+    @Test
+    void unitTest()
+    {
+        assertEquals(5, 5);
+    }
+}
